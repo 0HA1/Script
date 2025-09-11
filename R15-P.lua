22401f812d5291772cf5563ac667ddae98c9f221
0HA1 <111664341+0HA1@users.noreply.github.com>
2025-09-12 00:25:31 +0200

    Create R15-P.lua

---
+++ R15-P.lua
@@ -0,0 +1,18 @@
+return {
+    'Head',
+    'UpperTorso',
+    'LowerTorso',
+    'HumanoidRootPart',
+    'RightUpperArm',
+    'RightLowerArm',
+    'RightHand',
+    'LeftUpperArm',
+    'LeftLowerArm',
+    'LeftHand',
+    'RightUpperLeg',
+    'RightLowerLeg',
+    'RightFoot',
+    'LeftUpperLeg',
+    'LeftLowerLeg',
+    'LeftFoot'
+}
